6413e02269611d34d998fc6dca10f7b9ff616340
kayondomartin <kayondomartin45@gmail.com>
2023-05-25 03:32:01 +0900

    working on safe stack

--- llvm/lib/CodeGen/RustMetaSafeStack.cpp
+++ llvm/lib/CodeGen/RustMetaSafeStack.cpp
@@ -570,7 +570,7 @@ PreservedAnalyses MetaSafeStackPass::run(Function &F, FunctionAnalysisManager &F
   }
 
   auto getTM = [&]() -> TargetMachine* {
-    return FAM.getResult<TargetPassConfig>(F);
+    return &FAM.getResult<TargetPassConfig>().getTM<TargetMachine>();
   };
 
   auto getTLI = [&]() -> TargetLibraryInfo& {
